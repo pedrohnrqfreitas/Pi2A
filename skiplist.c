5e561def22e2c85fef433fba25b89f1716d3c1c7
pedrogomesleite <131780617+pedrogomesleite@users.noreply.github.com>
2023-05-29 15:29:59 -0300

    Função insert e Search
    função insert e search funcionado

--- skiplist.c
+++ skiplist.c
@@ -3,23 +3,23 @@
 #include <time.h>
 
 //define
-#define max_nivel 6
+#define max_level 6
 
 //structs
 typedef struct Node{
     int key;
-    struct Node* next[max_nivel];
+    struct Node* next[max_level];
 }Node;
 
 typedef struct skiplist{
-    int nivel;
-    Node* inicio[max_nivel];
+    int level;
+    Node* inicio[max_level];
 }Skiplist;
 
 //funções
-int rand_nivel();
+int rand_level();
 Skiplist* create_skiplist();
-Node* create_node(int key, int level);
+Node* create_node(int level);
 void insert_inicio(int key, Skiplist* lista);
 void insert(int key,  Skiplist *lista);
 void print_skiplist(Skiplist* skiplist);
@@ -28,11 +28,9 @@ int search(int key, Skiplist *lista);
 int main(){
     Skiplist* skiplist = create_skiplist();
 
-    //printf("%d\n", rand_nivel());
-
     insert(5, skiplist);
     insert(4, skiplist);
-    insert(3, skiplist);
+    insert(6, skiplist);
     insert(2, skiplist);
     insert(1, skiplist);
 
@@ -45,14 +43,14 @@ int main(){
     
 }
 
-int rand_nivel() {
+int rand_level() {
     static int initialized = 0;
     int num = 0;
     if (!initialized) {
         srand(time(NULL));
         initialized = 1;
     }
-    while(rand() % 2 == 1 && num < max_nivel){
+    while(rand() % 2 == 1 && num < max_level - 1){
         num++;
     }
     return num;
@@ -60,22 +58,22 @@ int rand_nivel() {
 
 Skiplist* create_skiplist(){
     Skiplist* skiplist = (Skiplist*) malloc(sizeof(Skiplist));
-    for(int i = 0; i < max_nivel; i++){
+    for(int i = 0; i < max_level; i++){
         skiplist->inicio[i] = NULL;
     }
     return skiplist;
 }
 
-Node* create_node(int key, int level){
+Node* create_node(int level){
     Node* newnode = (Node*) malloc(sizeof(Node));
-    newnode->key = key;
     return newnode;
 }
 
 void insert_inicio(int key, Skiplist* lista){
-    int level = rand_nivel();
-    Node* aux = create_node(key, level);
-    lista->nivel = level;
+    int level = rand_level();
+    Node* aux = create_node(level);
+    aux->key = key;
+    lista->level = level;
     for(int i = level; i >= 0; i--){
         aux->next[i] = lista->inicio[i];
         lista->inicio[i] = aux;
@@ -85,40 +83,28 @@ void insert_inicio(int key, Skiplist* lista){
 void insert(int key, Skiplist *lista) {
     if (lista->inicio[0] == NULL) {
         insert_inicio(key, lista);
-    } else {
-        int level = rand_nivel();
-        Node *novo = create_node(key, level);
-        Node *aux[max_nivel];
-        for(int i = 0; i < max_nivel; i++){
-            novo->next[i] = lista->inicio[i];
-        }
-        for(int i = max_nivel - 1; i >= 0; i--){
-            if(novo->next[i] == NULL){
-                aux[i] = novo;
+    } 
+    else {
+        int level = rand_level();
+        Node *novo = create_node(level);
+        novo->key = key;
+        for(int i = level; i >= 0; i--){
+            Node *aux = lista->inicio[i];
+            if(aux == NULL){
+                novo->next[i] = NULL;
+                lista->inicio[i] = novo;
                 continue;
             }
+            if(aux->key > key){
+                novo->next[i] = lista->inicio[i];
+                lista->inicio[i] = novo;
+            }
             else{
-                while (novo->next[i]->key < key){
-                    novo = novo->next[i];
+                while (aux->next[i] != NULL && aux->next[i]->key <= key){
+                    aux = aux->next[i];
                 }
-                aux[i] = novo;
-            }
-        }
-        novo = novo->next[0];
-        if(novo->key == key){
-            //atualização do valor
-        }
-        else{
-            if(lista->nivel < level){
-                for(int i = level + 1; i < max_nivel; i++){
-                    aux[i] = lista->inicio[i];
-                }
-                lista->nivel = level;
-            }
-            for(int i = 0; i < level; i++){
-                novo->next[i] = aux[i]->next[i];
-                aux[i]->next[i] = novo;
-
+                novo->next[i] = aux->next[i];
+                aux->next[i] = novo; 
             }
         }
     }
@@ -126,30 +112,34 @@ void insert(int key, Skiplist *lista) {
 
 
 void print_skiplist(Skiplist* skiplist){
-    for(int i = max_nivel - 1; i >= 0; i--){
+    for(int i = max_level - 1; i >= 0; i--){
         Node* aux = skiplist->inicio[i];
         printf("Level %d: ", i);
         while(aux != NULL){
             printf("%d ", aux->key);
             aux = aux->next[i];
+            
+        }
+        if(aux == NULL){
+            printf("NULL");
         }
         printf("\n");
     }
 }
 
 int search(int key, Skiplist *lista){
-    Node *aux;
-    aux->key = 0;
-    for(int i = 0; i < max_nivel; i++){
-        aux->next[i] = lista->inicio[i];
-    }
-    for(int i = max_nivel - 1; i >= 0; i--){
-        while(aux->key != key || aux->next[i] != NULL){
-            printf("%d\n ", aux->key);
-            aux = aux->next[i];
+    for(int i = max_level - 1; i >= 0; i--){
+        Node *aux = lista->inicio[i];
+        if(aux == NULL){
+            continue;
         }
-        if(aux->key == key){
-            return 1;
+        else{
+            while(aux != NULL && aux->key <= key){
+                if(aux->key == key){
+                return 1;
+                }
+                aux = aux->next[i];
+            }
         }
     }
     return 0;
